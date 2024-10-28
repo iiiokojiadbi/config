10ef4f274851389b4dd617fc300b8a32e9426080
iiiokojiadbi <iiiokojiadbi@yandex.ru>
2024-10-29 00:06:27 +0300

    add nvim core config

---
+++ nv/lua/core/config.lua
@@ -0,0 +1,17 @@
+-- Относительные строки
+vim.wo.number = true
+vim.wo.relativenumber = true
+
+-- Общая вставка
+vim.opt.clipboard = "unnamedplus"
+
+-- Отступы
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+
+-- Скролл
+vim.opt.scrolloff = 8
+vim.opt.wrap = false
+vim.opt.termguicolors = true
+
