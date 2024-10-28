0d79b59832e82e97f4c8db0d24e9f8dcb518eeb5
iiiokojiadbi <iiiokojiadbi@yandex.ru>
2024-10-29 00:49:25 +0300

    edit wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,12 +1,10 @@
 -- Pull in the wezterm API
 local wezterm = require("wezterm")
+local mux = wezterm.mux
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices
-
--- For example, changing the color scheme:
 config.font = wezterm.font("JetBrainsMono Nerd Font")
 config.font_size = 16
 
@@ -18,9 +16,15 @@ config.macos_window_background_blur = 20
 config.window_padding = {
 	left = 0,
 	right = 0,
-	top = 45,
+	top = 5,
 	bottom = 0,
 }
+
+-- Старт на весь экран
+--wezterm.on("gui-startup", function(cmd)
+--    local tab, pane, window = mux.spawn_window(cmd or {})
+--    window:gui_window():maximize()
+--end)
 
 config.keys = {
 	{
@@ -28,6 +32,32 @@ config.keys = {
 		mods = "SHIFT|CTRL",
 		action = wezterm.action.ToggleFullScreen,
 	},
+	{
+		key = "_",
+    mods = "SHIFT|CTRL|ALT",
+    action = wezterm.action.InputSelector {
+      title = 'Выберите тип окна:',
+      choices = {
+        {
+          id = 'splitHorizontal',
+          label = 'горизонтальное окно'
+        },
+        {
+          id = 'splitVertical',
+          label = 'вертикальное окно'
+        },
+      },
+      action = wezterm.action_callback(function(window, pane, id, label)
+        if not id then
+          wezterm.log_info 'Nothing selected'
+        elseif id == 'splitHorizontal' then
+          window:perform_action(wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' }, pane)
+        else
+          
+        end
+      end),
+  },},
 }
+
 -- and finally, return the configuration to wezterm
 return config
