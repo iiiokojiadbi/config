deba2433b5945f185b5a51b0bc8ba554e500061a
iiiokojiadbi <iiiokojiadbi@yandex.ru>
2024-10-28 09:37:53 +0300

    initial commit

---
+++ nv/lua/plugins/lsp.lua
@@ -0,0 +1,43 @@
+return {
+  -- MASON
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "stylua",
+        "tailwindcss-language-server",
+        "lua-language-server",
+        "html-lsp",
+        "prettier",
+        "eslint_d",
+        "rust-analyzer",
+        "json-lsp",
+        "css-lsp",
+      },
+    },
+  },
+  -- LSP
+  {
+    "neovim/nvim-lspconfig",
+    ---@class PluginLspOpts
+    opts = {
+      ---@type lspconfig.options
+      servers = {
+        -- tsserver will be automatically installed with mason and loaded with lspconfig
+        tsserver = {},
+      },
+      -- you can do any additional lsp server setup here
+      -- return true if you don't want this server to be setup with lspconfig
+      ---@type table<string, fun(server:string, opts:_.lspconfig.options):boolean?>
+      setup = {
+        -- example to setup with typescript.nvim
+        tsserver = function(_, opts)
+          require("typescript").setup({ server = opts })
+          return true
+        end,
+        -- Specify * to use this function as a fallback for any server
+        -- ["*"] = function(server, opts) end,
+      },
+    },
+  },
+}
