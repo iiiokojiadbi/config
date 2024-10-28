b4fa8bde49466ef7201674964496631f02efab1d
iiiokojiadbi <iiiokojiadbi@yandex.ru>
2024-10-28 21:40:32 +0300

    edit wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -8,11 +8,26 @@ local config = wezterm.config_builder()
 
 -- For example, changing the color scheme:
 config.font = wezterm.font("JetBrainsMono Nerd Font")
-config.font_size = 14
+config.font_size = 16
 
 config.enable_tab_bar = false
 config.color_scheme = "GruvboxDark"
 config.window_background_opacity = 0.9
 config.macos_window_background_blur = 20
+
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 10,
+	bottom = 0,
+}
+
+config.keys = {
+	{
+		key = "n",
+		mods = "SHIFT|CTRL",
+		action = wezterm.action.ToggleFullScreen,
+	},
+}
 -- and finally, return the configuration to wezterm
 return config
