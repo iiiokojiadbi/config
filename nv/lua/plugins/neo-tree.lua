92f19bfbc955061254ca13406ca649d150c7747b
iiiokojiadbi <iiiokojiadbi@yandex.ru>
2024-11-01 01:34:18 +0300

    add lsp, add mason, add ts-comments, add cmp, edit bufferline, edit neo-tree

--- nv/lua/plugins/neo-tree.lua
+++ nv/lua/plugins/neo-tree.lua
@@ -5,5 +5,19 @@ return {
       "nvim-lua/plenary.nvim",
       "nvim-tree/nvim-web-devicons",
 			"MunifTanjim/nui.nvim",
-    }
+    },
+		config = function()
+
+	local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+	
+			require("notify")(hl)
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end			
+
+		require("neo-tree").setup({
+			close_if_last_window = false,
+		})
+	end,
 }
